6783da64e68727a3f881d3ad0dec17757f4a270e
sarvathul basha <basha@uitouxsolutions.com>
2018-09-27 17:35:51 +0530

    new branch test Signed-off-by: sarvathul basha <basha@uitouxsolutions.com>
    Signed-off-by: sarvathul basha <basha@uitouxsolutions.com>

--- app/src/main/java/com/google/firebase/example/fireeats/activities/RegistrationActivity.java
+++ app/src/main/java/com/google/firebase/example/fireeats/activities/RegistrationActivity.java
@@ -132,8 +132,6 @@ public class RegistrationActivity extends AppCompatActivity {
 
         rl_back_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
-
-
             public void onClick(View view) {
                 onBackPressed();
             }
